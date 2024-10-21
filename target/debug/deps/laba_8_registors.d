d96a60713cd51b1f079e4bcc07d540bd6b17fc26
AndreyDmytrenko <dmitrenkoandrey41@gmail.com>
2024-10-21 09:34:31 +0300

    add project

---
+++ target/debug/deps/laba_8_registors.d
@@ -0,0 +1,5 @@
+C:\Users\user\laba_8_registors\target\debug\deps\laba_8_registors.exe: src/main.rs
+
+C:\Users\user\laba_8_registors\target\debug\deps\laba_8_registors.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/laba_8_registors.d
@@ -0,0 +1,1 @@
+C:\Users\user\laba_8_registors\target\debug\laba_8_registors.exe: C:\Users\user\laba_8_registors\src\main.rs
